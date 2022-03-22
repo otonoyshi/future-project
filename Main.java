f1e6e51315434418249c70f3dd1b52b7f00edb50
Georgy <georgy.matskevich@gmail.com>
2022-03-22 12:52:07 +0300

    payment

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
+class Main {
+    public static void main (String[] args) {
         System.out.println("Привет");
+        Payment payment = new Payment;
+        System.out.println(3);
     }
 }
